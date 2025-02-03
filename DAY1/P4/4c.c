0bdeacd7fe59148eb154dd96ef703dea8e7235a0
KoYa2003 <115389781+KoYa2003@users.noreply.github.com>
2025-02-03 22:22:37 +0530

    Update 4c.c

--- DAY1/P4/4c.c
+++ DAY1/P4/4c.c
@@ -1,3 +1,14 @@
+//Dimond Star Pattern
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
 #include<stdio.h>
 int main(){
     int i,j;
@@ -6,12 +17,29 @@ int main(){
         for(j=i;j<=n-1;j++){
         printf(" ");
         }
-        printf("\n"); 
+         
+        for(j=1;j<i;j++){
+            printf("*");
+        }
+         
         for(j=1;j<=i;j++){
             printf("*");
         }
-          
+    printf("\n");      
     }
-    printf("\n");
+    for (i=n-1;i>0;i--){
+        for(j=i;j<=n-1;j++){
+        printf(" ");
+        }
+         
+        for(j=1;j<i;j++){
+            printf("*");
+        }
+         
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+    printf("\n");      
+    }
    
 }    
