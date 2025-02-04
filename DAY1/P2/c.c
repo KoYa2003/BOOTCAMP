f1745b48b4f3ffaea0c9a5025d5d852e12e352d1
KoYa2003 <115389781+KoYa2003@users.noreply.github.com>
2025-02-04 21:51:52 +0530

    Update c.c

--- DAY1/P2/c.c
+++ DAY1/P2/c.c
@@ -1,17 +1,25 @@
+//Character Pyramid
+//     A
+//    ABA
+//   ABCBA
+//  ABCDCBA
+// ABCDEDCBA
+
 #include <stdio.h>
 
 int main(){
     int i,j;
-    char ch='A';
     for(i=1;i<=5;i++){
-        for(j=i;j<=5;j++){
+        char ch='A';
+        for(j=i;j<5;j++){
             printf(" ");
         }
-        for(j=1;j<i;j++){
-            printf("%c",ch);
+        for(j=1;j<=i;j++){
+            printf("%c",ch++);
         }
-        for(j=i;j>0;j--){
-            printf("%c",ch);
+        ch -= 2; 
+        for(j=1;j<i;j++){
+            printf("%c",ch--);
         }
         //ch++;
 
