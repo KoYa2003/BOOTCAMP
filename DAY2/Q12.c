bc98bc49bf306502c0afe7492484112539008176
KoYa2003 <115389781+KoYa2003@users.noreply.github.com>
2025-02-05 22:46:17 +0530

    Update Q12.c

--- DAY2/Q12.c
+++ DAY2/Q12.c
@@ -1,3 +1,5 @@
+//program to check if a number is an Armstrong number 
+
 #include <math.h>
 #include <stdio.h>
 
@@ -5,22 +7,22 @@ int main() {
     int n, r, d = 0, sum = 0;
     printf("Enter a number: ");
     scanf("%d", &n);
-    int m = n;
+    int m=n;
 
-    while (n != 0) {
-        d = d + 1;
-        n = n / 10;
+    while (n!=0) {
+        d=d+1;
+        n=n/10;
     }
 
-    n = m; 
+    n=m; 
 
-    while (n != 0) {
-        r = n % 10;
-        sum = sum + (int)pow(r, d);
-        n = n / 10; 
+    while (n!=0) {
+        r=n%10;
+        sum=sum+(int)pow(r, d);
+        n=n/10; 
     }
 
-    if (m == sum) {
+    if (m==sum) {
         printf("Number is Armstrong!");
     } else {
         printf("Number is not Armstrong!");
