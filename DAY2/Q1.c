b27a43a82e98bc7412e85eb074163074506ba0d6
KoYa2003 <115389781+KoYa2003@users.noreply.github.com>
2025-02-05 22:13:01 +0530

    Update Q1.c

--- DAY2/Q1.c
+++ DAY2/Q1.c
@@ -1,25 +1,20 @@
-#include<stdio.h>
-int main(){
-    int a,b,c;
-    printf("Enter a.b.c");
-    scanf("%d %d %d",&a,&b,&c);
+//Program to check if a number is positive, negative, or zero 
 
-    //using for loop
-    // if (a>b){
-    //     if(a>c){
-    //         printf("A is greater");
-    //     }
-    //     else {
-    //         printf("C is greater");
-    //     }
-    // }
-    // else{ 
-    //     if (b>c){
-    //     printf("B is greater");
-    //     }
-    //     else {
-    //         printf("C is greater");
-    //     }
+#include <stdio.h>
 
-    //ternaryloop
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n > 0) {
+        printf("The number is positive");
+    } else if (n < 0) {
+        printf("The number is negative");
+    } else {
+        printf("The number is zero");
+    }
+
+    return 0;
 }
