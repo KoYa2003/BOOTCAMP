5852346ac49996726ba8e9d72e7c6956a8d6820f
KoYa2003 <115389781+KoYa2003@users.noreply.github.com>
2025-02-08 17:04:39 +0530

    Add files via upload

---
+++ DAY1/P4/4h.c
@@ -0,0 +1,35 @@
+//Left arrow star pattern
+
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++){
+        for(j=i;j<n;j++){
+            printf(" ");
+        }
+        
+        //for(j=0;j<n-i;j++){
+        for(j=i;j<n;j++){
+            printf("*");
+        }
+        printf("\n");
+    }  
+
+    for(i=1;i<n;i++){
+        for(j=1;j<=i;j++){
+            printf(" ");
+        }
+        
+        for(j=0;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }  
+        
+        printf("\n");
+    
+    return 0;
+}
