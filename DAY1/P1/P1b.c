1c71be4838da61376a53fbe137d77f3f8aab881a
KoYa2003 <115389781+KoYa2003@users.noreply.github.com>
2025-02-08 15:33:59 +0530

    Update P1b.c

--- DAY1/P1/P1b.c
+++ DAY1/P1/P1b.c
@@ -4,15 +4,18 @@
 // ***
 // **
 // *
-#include <stdio.h>
-
+#include<stdio.h>
 int main(){
-    int i,j;
-    for(i=1;i<=5;i++){
-        for(j=i;j<=5;j++){
+    int i,j,n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++){
+        //for(j=0;j<n-i;j++){
+        for(j=i;j<n;j++){
             printf("*");
         }
         printf("\n");
     }
-
+    return 0;
 }
